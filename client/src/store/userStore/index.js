62d0c24c5172022608f7f613002811567cf82494
Alireza Heidari <itisalirh@gmail.com>
2023-03-07 16:54:35 +0100

    dorp importing vuex userStore

--- client/src/store/userStore/index.js
+++ client/src/store/userStore/index.js
@@ -1,4 +1,3 @@
-export { userStore } from "./userStore";
 export { default as User } from "./User";
 export { syncUserToGalaxy } from "./syncUserToGalaxy";
 export { userFlagsStore } from "./userFlagsStore";
