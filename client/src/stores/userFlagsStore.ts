4fa141cafc8669916991767f3faf3ee3cee6d2dd
Alireza Heidari <itisalirh@gmail.com>
2023-03-10 16:44:51 +0100

    convert userFlagsStore to pinia composition API

--- client/src/stores/userFlagsStore.ts
+++ client/src/stores/userFlagsStore.ts
@@ -1,12 +1,15 @@
+import { ref } from "vue";
 import { defineStore } from "pinia";
 
-export const useUserFlagsStore = defineStore("userFlagsStore", {
-    state: () => ({
-        showSelectionQueryBreakWarning: true,
-    }),
-    actions: {
-        ignoreSelectionQueryBreakWarning() {
-            this.showSelectionQueryBreakWarning = false;
-        },
-    },
+export const useUserFlagsStore = defineStore("userFlagsStore", () => {
+    const showSelectionQueryBreakWarning = ref(true);
+
+    function ignoreSelectionQueryBreakWarning() {
+        showSelectionQueryBreakWarning.value = false;
+    }
+
+    return {
+        showSelectionQueryBreakWarning,
+        ignoreSelectionQueryBreakWarning,
+    };
 });
