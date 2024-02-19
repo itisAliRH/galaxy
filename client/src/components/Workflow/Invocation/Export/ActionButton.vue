9fe72a1381421ac12e2ee74c9d21d149a0751686
Alireza Heidari <itisalirh@gmail.com>
2024-02-19 17:03:30 +0100

    🛠️: import missing components, refactor to `typeScript` and typing improvements in `ActionButton`

--- client/src/components/Workflow/Invocation/Export/ActionButton.vue
+++ client/src/components/Workflow/Invocation/Export/ActionButton.vue
@@ -1,22 +1,26 @@
-<script setup>
+<script setup lang="ts">
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { BButton } from "bootstrap-vue";
 import { ref } from "vue";
 
-import { InvocationExportPluginAction } from "./model";
+import { type InvocationExportPluginAction } from "./model";
 
 const modal = ref(null);
 
-const props = defineProps({
-    action: { type: InvocationExportPluginAction, required: true },
-});
+interface Props {
+    action: InvocationExportPluginAction;
+}
+
+const props = defineProps<Props>();
 </script>
 
 <template>
-    <b-button v-b-tooltip.hover.bottom :title="props.action.title" @click="props.action.run(modal)">
+    <BButton v-b-tooltip.hover.bottom :title="props.action.title" @click="props.action.run(modal)">
         <FontAwesomeIcon v-if="props.action.icon" :icon="props.action.icon" />
         <div v-else>
             {{ props.action.title }}
         </div>
+
         <component :is="props.action.modal" v-if="props.action.modal" ref="modal" />
-    </b-button>
+    </BButton>
 </template>
